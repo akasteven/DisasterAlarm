3f17b1ec811b353ccfe760fd2de06999b0862885
creolophus <stevenqiucn@gmail.com>
2013-09-23 11:29:38 +0800

    release the spider when tomcat starts

---
+++ Spider/src/spider/daemon.java
@@ -0,0 +1,40 @@
+package spider;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class daemon extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public daemon() {
+		super();
+	}
+
+
+	public void destroy() {
+		super.destroy(); 
+	}
+
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {		
+		super.doPost(request, response);
+	}
+
+
+	public void init() throws ServletException {
+		 new Scheduler(30*60);
+	}
+
+}
